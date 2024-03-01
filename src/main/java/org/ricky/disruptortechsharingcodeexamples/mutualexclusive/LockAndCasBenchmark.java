cf4eb793720d9847c9baac48ebe50fd1c823ff47
Rikikudo <trunghieu99tt@gmail.com>
2024-03-02 02:04:57 +0700

    feat: add CAS benchmark
    - rename lock benchmark file

--- src/main/java/org/ricky/disruptortechsharingcodeexamples/mutualexclusive/LockAndCasBenchmark.java
+++ src/main/java/org/ricky/disruptortechsharingcodeexamples/mutualexclusive/LockAndCasBenchmark.java
@@ -1,4 +1,4 @@
-package org.ricky.disruptortechsharingcodeexamples.locks;
+package org.ricky.disruptortechsharingcodeexamples.mutualexclusive;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Scope;
@@ -13,14 +13,16 @@ import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 @State(Scope.Benchmark)
-public class LockBenchmark {
+public class LockAndCasBenchmark {
 
   private long counter = 0;
   private final Lock lock = new ReentrantLock();
+  private final AtomicLong atomicCounter = new AtomicLong();
 
   @Benchmark
   @BenchmarkMode(Mode.Throughput)
@@ -54,9 +56,24 @@ public class LockBenchmark {
     }
   }
 
+  @Benchmark
+  @BenchmarkMode(Mode.Throughput)
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  public void singleThreadIncrementWithCAS() {
+    atomicCounter.incrementAndGet();
+  }
+
+  @Benchmark
+  @BenchmarkMode(Mode.Throughput)
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  @Threads(2)
+  public void twoThreadsIncrementWithCAS() {
+    atomicCounter.incrementAndGet();
+  }
+
   public static void main(String[] args) throws RunnerException {
     Options opt = new OptionsBuilder()
-        .include(LockBenchmark.class.getSimpleName())
+        .include(LockAndCasBenchmark.class.getSimpleName())
         .forks(1)
         .build();
 
